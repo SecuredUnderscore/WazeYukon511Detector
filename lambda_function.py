4d7652afd92d5d89f50a1929c3fa45846bdeb461
Jaxon <bjornsonjaxon@gmail.com>
2025-02-19 23:33:25 -0800

    Fixed Georss Boundaries

--- lambda_function.py
+++ lambda_function.py
@@ -6,8 +6,8 @@ import os
 import boto3
 
 check_feed_delay = 60
-# feed_url = "https://www.waze.com/live-map/api/georss?env=na&ofa=true&ma=600&mj=100&mu=100&types=traffic&left=-126.41694836083697&top=47.7818471020394&right=-123.01118664208425&bottom=50.69999171901272"
-feed_url = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100&types=traffic&left=-143.38835601265083&top=48.53088447151348&right=-106.38640288765265&bottom=67.07583312322939"
+feed_url = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100&types=traffic&left=-133&top=53&right=-114&bottom=48"
+feed_url2 = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100&types=traffic&left=-133&top=58&right=-114&bottom=53"
 discord_webhook_url = os.environ['DISCORD_WEBHOOK_URL']
 discord_webhook_log_url = os.environ['DISCORD_WEBHOOK_LOG_URL']
 
@@ -42,13 +42,44 @@ async def start():
 
     parsed_api = api.json()
 
+
+    try:
+        api2 = requests.get(feed_url2)
+    except ConnectionError:
+        await send_log("Unable to connect to api")
+        return
+
+    print(api.status_code)
+    if api.status_code != 200:
+        await send_log(f"API Response Code {api.status_code}")
+        return
+
+    parsed_api2 = api2.json()
+
+
     uuids2: dict = table_active.scan()['Items']
     uuids = []
     for uuid in uuids2:
         uuids.append(uuid.get('uuid'))
 
     for event in parsed_api['jams']:
+        try:
+            uuid = str(event['uuid'])
+            reporter = event['causeAlert']['reportBy']
 
+            if reporter == 'Yukon 511':
+                if not uuid in uuids:
+                    print(f"Adding {uuid}")
+                    table_active.put_item(Item={'uuid': str(uuid)})
+                    await send_webhook(event)
+                try:
+                    uuids.remove(uuid)
+                except ValueError:
+                    pass
+        except KeyError:
+            pass
+
+    for event in parsed_api2['jams']:
         try:
             uuid = str(event['uuid'])
             reporter = event['causeAlert']['reportBy']
