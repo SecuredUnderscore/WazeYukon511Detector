8a55300b5626a9c2ce191bb5820d90939176416c
Jaxon <bjornsonjaxon@gmail.com>
2025-02-21 13:49:58 -0800

    Make sure only 1 notification per unique closure

--- lambda_function.py
+++ lambda_function.py
@@ -10,6 +10,7 @@ feed_url = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100
 feed_url2 = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100&types=traffic&left=-133&top=58&right=-114&bottom=53"
 discord_webhook_url = os.environ['DISCORD_WEBHOOK_URL']
 discord_webhook_log_url = os.environ['DISCORD_WEBHOOK_LOG_URL']
+uuids = []
 
 def lambda_handler(event, context):
     loop = asyncio.new_event_loop()
@@ -27,7 +28,10 @@ async def start():
     # Create database clients
     dynamodb = boto3.resource('dynamodb')
     database_name_active = os.environ['DB_NAME_ACTIVE']
+    global table_active
     table_active = dynamodb.Table(database_name_active)
+
+
 
     try:
         api = requests.get(feed_url)
@@ -40,9 +44,6 @@ async def start():
         await send_log(f"API Response Code {api.status_code}")
         return
 
-    parsed_api = api.json()
-
-
     try:
         api2 = requests.get(feed_url2)
     except ConnectionError:
@@ -54,53 +55,41 @@ async def start():
         await send_log(f"API Response Code {api.status_code}")
         return
 
-    parsed_api2 = api2.json()
 
 
     uuids2: dict = table_active.scan()['Items']
-    uuids = []
     for uuid in uuids2:
         uuids.append(uuid.get('uuid'))
 
-    for event in parsed_api['jams']:
-        try:
-            uuid = str(event['uuid'])
-            reporter = event['causeAlert']['reportBy']
+    parsed_api = api.json()
+    parsed_api2 = api2.json()
 
-            if reporter == 'Yukon 511':
-                if not uuid in uuids:
-                    print(f"Adding {uuid}")
-                    table_active.put_item(Item={'uuid': str(uuid)})
-                    await send_webhook(event)
-                try:
-                    uuids.remove(uuid)
-                except ValueError:
-                    pass
-        except KeyError:
-            pass
+    await check_for_yukon(parsed_api)
+    await check_for_yukon(parsed_api2)
 
-    for event in parsed_api2['jams']:
-        try:
-            uuid = str(event['uuid'])
-            reporter = event['causeAlert']['reportBy']
-
-            if reporter == 'Yukon 511':
-                if not uuid in uuids:
-                    print(f"Adding {uuid}")
-                    table_active.put_item(Item={'uuid': str(uuid)})
-                    await send_webhook(event)
-                try:
-                    uuids.remove(uuid)
-                except ValueError:
-                    pass
-        except KeyError:
-            pass
-
-    for uuid in uuids:
-        print(f"Removing {uuid}")
-        table_active.delete_item(Key={'uuid': uuid})
+    # for uuid in uuids:
+    #     print(f"Removing {uuid}")
+    #     table_active.delete_item(Key={'uuid': uuid})
 
     await send_log("Script Completed")
+
+async def check_for_yukon(parsed_api):
+    for event in parsed_api['jams']:
+        try:
+            uuid = str(event['causeAlert']['id'])
+            reporter = event['causeAlert']['reportBy']
+
+            if reporter == 'Yukon 511':
+                if not uuid in uuids:
+                    print(f"Adding {uuid}")
+                    table_active.put_item(Item={'uuid': str(uuid)})
+                    await send_webhook(event)
+                # try:
+                #     uuids.remove(uuid)
+                # except ValueError:
+                #     pass
+        except KeyError:
+            pass
 
 async def send_webhook(event):
     async with aiohttp.ClientSession() as session:
