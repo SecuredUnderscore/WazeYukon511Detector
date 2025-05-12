ad2beb9ec6c51de8fc8c0d9e805e79e8ce677242
Jaxon-Bjornson <bjornson@pde.com>
2025-05-11 23:15:16 -0700

    This script is now a crash detector

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,7 @@ import aiohttp
 import requests
 import os
 import boto3
+from boto3.dynamodb.conditions import Key
 
 check_feed_delay = 60
 feed_url = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100&types=traffic&left=-133&top=53&right=-114&bottom=48"
@@ -52,7 +53,7 @@ async def start():
 
 async def do_full_api_check(left, right, top, bottom):
     try:
-        url = f"https://www.waze.com/live-map/api/georss?env=na&types=traffic&left={left}&top={top}&right={right}&bottom={bottom}"
+        url = f"https://www.waze.com/live-map/api/georss?env=na&format=1&types=traffic&atf=ACCIDENT,JAM&left={left}&top={top}&right={right}&bottom={bottom}"
         print(f"Loading {url}")
         api = requests.get(url)
     except ConnectionError:
@@ -73,30 +74,73 @@ async def check_for_yukon(parsed_api):
     except KeyError:
         return
 
+    jamUuids = {}
+
     for event in jams:
         try:
-            uuid = str(event['causeAlert']['id'])
-            reporter = event['causeAlert']['reportBy']
+            uuid = str(event['causeAlert']['uuid'])
+            event_type = str(event['causeAlert']['type'])
+            road_type = str(event['causeAlert']['roadType'])
+            street_name = str(event['causeAlert']['street'])
+            city = str(event['causeAlert']['city'])
 
-            if reporter == "Yukon 511":
-                print(f"Found Yukon 511 for {uuid}")
+            if event_type == "ACCIDENT" and (road_type == 2 or road_type == 3 or road_type == 4): # Freeways or Ramps or Primary Streets
                 if not uuid in uuids:
                     print(f"Adding {uuid}")
                     table_active.put_item(Item={'uuid': str(uuid)})
-                    await send_webhook(event)
-                # try:
-                #     uuids.remove(uuid)
-                # except ValueError:
-                #     pass
+                    await send_webhook(event, 0, f"Crash at {street_name}, {city}")
+
+                try:
+                    jamUuid = event['causeAlert']['jamUuid']
+                    table_active.put_item(Item={'uuid': str(uuid), 'jamUuid': jamUuid})
+
+                    jamUuids[jamUuid] = {
+                        'uuid': uuid,
+                        'street_name': street_name,
+                        'city': city
+                    }
+                except KeyError:
+                    pass
+
         except KeyError:
             pass
 
-async def send_webhook(event):
-    async with aiohttp.ClientSession() as session:
-        embed = discord.Embed(title=f"New Yukon511 Closure")
-        embed.add_field(name="Road", value=event['street'])
+    for event in jams:
+        try:
+            uuid = str(event['uuid'])
+            if uuid in jamUuids:
+                crash_uuid = jamUuids[uuid]['uuid']
+                street_name = jamUuids[uuid]['street_name']
+                city = jamUuids[uuid]['city']
+                speedKMH = str(event['speedKMH'])
 
-        embed.add_field(name="Links", value=f"https://www.waze.com/en-US/editor?env=usa&lat={event['causeAlert']['location']['y']}&lon={event['causeAlert']['location']['x']}&zoomLevel=16&segments={event['segments'][0]['ID']}")
+                response = table_active.query(
+                    KeyConditionExpression=Key('uuid').eq(crash_uuid),
+                    Limit=1
+                )
+                items = response.get('Items', [])
+                prev_speed = None
+                try:
+                    prev_speed = items[0]['speedKMH']
+                except KeyError:
+                    pass
+
+                table_active.put_item(Item={'uuid': str(crash_uuid), 'jamUuid': uuid, 'speedKMH': speedKMH})
+                if prev_speed is None and int(speedKMH) < 5:
+                    await send_webhook(event, 1, f"Crash at {street_name}, {city} is {speedKMH}km/h due to a crash")
+                elif prev_speed is None:
+                    pass
+                elif (prev_speed - speedKMH) > 1 and int(speedKMH) < 5:
+                    await send_webhook(event, 1, f"Crash at {street_name}, {city} is now reduced to {speedKMH}km/h due to a crash")
+
+        except KeyError:
+            pass
+
+async def send_webhook(event, notify_type, message = ""):
+    async with aiohttp.ClientSession() as session:
+        embed = discord.Embed(title=message)
+        if notify_type == 0:
+            embed.add_field(name="Links", value=f"https://www.waze.com/en-US/editor?env=usa&lat={event['causeAlert']['location']['y']}&lon={event['causeAlert']['location']['x']}&zoomLevel=16")
         webhook = discord.Webhook.from_url(discord_webhook_url, session=session)
         await webhook.send(embed=embed)
 
