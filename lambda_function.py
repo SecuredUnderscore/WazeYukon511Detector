147b09047109153c30714e5ed7b46ecf6dbbe51e
SecuredUnderscore <bjornsonjaxon@gmail.com>
2025-09-08 14:56:16 -0700

    added user closure detection

--- lambda_function.py
+++ lambda_function.py
@@ -4,15 +4,17 @@ import aiohttp
 import requests
 import os
 import boto3
-from boto3.dynamodb.conditions import Key
 
 check_feed_delay = 60
 feed_url = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100&types=traffic&left=-133&top=53&right=-114&bottom=48"
 feed_url2 = "https://www.waze.com/live-map/api/georss?env=na&ma=600&mj=100&mu=100&types=traffic&left=-133&top=58&right=-114&bottom=53"
 discord_webhook_url = os.environ['DISCORD_WEBHOOK_URL']
 discord_webhook_log_url = os.environ['DISCORD_WEBHOOK_LOG_URL']
+pushover_api_token = os.environ['PUSHOVER_API_TOKEN']
+pushover_user_key = os.environ['PUSHOVER_USER_KEY']
 uuids = []
 road_names = ["Hwy 1", "Hwy 4", "Hwy 14", "Hwy 17", "Hwy 18", "Hwy 19", "Hwy 4a", "Hwy 1a", "Nanaimo Pky", "Sooke Rd"]
+reporter_whitelist = ["District of Oak Bay", "City of Langford", "RyckNe", "Trexer0", "ThatVictoriaGuy", "BryceCampbell"]
 
 def lambda_handler(event, context):
     loop = asyncio.new_event_loop()
@@ -37,7 +39,7 @@ async def start():
     for uuid in uuids2:
         uuids.append(uuid.get('uuid'))
 
-    # # await do_full_api_check(-122.333, -122.308, 37.934, 37.915)
+    # await do_full_api_check(-122.333, -122.308, 37.934, 37.915)
     # await do_full_api_check(-128.4, -122.6, 51.0, 48.0)
     # await do_full_api_check(-122.6, -116.8, 51.0, 48.0)
     # await do_full_api_check(-128.4, -122.6, 54.0, 51.0)
@@ -89,15 +91,14 @@ async def check_for_yukon(parsed_api):
     except KeyError:
         return
 
-    jamUuids = {}
-
     for event in jams:
         try:
-            uuid = str(event['id'])
-            event_type = str(event['type'])
-            road_type = str(event['roadType'])
-            street_name = str(event['street'])
-            city = str(event['city'])
+            uuid = event.get('id', None)
+            event_type = event.get('type', None)
+            road_type = event.get('roadType', None)
+            street_name = event.get('street', None)
+            city = event.get('city', None)
+            reporter = event.get('reportBy', None)
 
             # if event_type == "ACCIDENT" and (road_type == 2 or road_type == 3 or road_type == 4 or road_type == 1): # Freeways or Ramps or Primary Streets
             if event_type == "ACCIDENT" and any(name.lower() in street_name.lower() for name in road_names): # Freeways or Ramps or Primary Streets
@@ -106,57 +107,54 @@ async def check_for_yukon(parsed_api):
                     table_active.put_item(Item={'uuid': str(uuid)})
                     await send_webhook(event, 0, f"Crash at {street_name}, {city}")
 
-                # try:
-                #     jamUuid = event['causeAlert']['jamUuid']
-                #     table_active.put_item(Item={'uuid': str(uuid), 'jamUuid': jamUuid})
-                #
-                #     jamUuids[jamUuid] = {
-                #         'uuid': uuid,
-                #         'street_name': street_name,
-                #         'city': city
-                #     }
-                # except KeyError:
-                #     pass
-
+            if event_type == "ROAD_CLOSED" and reporter not in reporter_whitelist and (road_type == 3 or road_type == 4 or road_type == 6 or road_type == 7):
+                if not uuid in uuids:
+                    print(f"Adding {uuid}")
+                    uuids.append(uuid)
+                    table_active.put_item(Item={'uuid': str(uuid)})
+                    await send_webhook(event, 1, f"New user closure: {street_name}, {city} ({roadtype_to_string(road_type)})")
         except KeyError:
             pass
 
-    # for event in jams:
-    #     try:
-    #         uuid = str(event['uuid'])
-    #         if uuid in jamUuids:
-    #             crash_uuid = jamUuids[uuid]['uuid']
-    #             street_name = jamUuids[uuid]['street_name']
-    #             city = jamUuids[uuid]['city']
-    #             speedKMH = str(event['speedKMH'])
-    #
-    #             response = table_active.query(
-    #                 KeyConditionExpression=Key('uuid').eq(crash_uuid),
-    #                 Limit=1
-    #             )
-    #             items = response.get('Items', [])
-    #             prev_speed = None
-    #             try:
-    #                 prev_speed = items[0]['speedKMH']
-    #             except KeyError:
-    #                 pass
-    #
-    #             table_active.put_item(Item={'uuid': str(crash_uuid), 'jamUuid': uuid, 'speedKMH': speedKMH})
-    #             if prev_speed is None and int(speedKMH) < 5:
-    #                 await send_webhook(event, 1, f"Traffic at {street_name}, {city} is {speedKMH}km/h due to a crash")
-    #             elif prev_speed is None:
-    #                 pass
-    #             elif (prev_speed - speedKMH) > 1 and int(speedKMH) < 5:
-    #                 await send_webhook(event, 1, f"Traffic at {street_name}, {city} is now reduced to {speedKMH}km/h due to a crash")
-    #
-    #     except KeyError:
-    #         pass
+def roadtype_to_string(road_type: int) -> str:
+    if road_type == 1:
+        return "Street"
+    elif road_type == 2:
+        return "Primary Street"
+    elif road_type == 3:
+        return "Freeway"
+    elif road_type == 4:
+        return "Ramp"
+    elif road_type == 5:
+        return "Trail"
+    elif road_type == 6:
+        return "Major Highway"
+    elif road_type == 7:
+        return "Minor Highway"
+    elif road_type == 17:
+        return "Private Road"
+    elif road_type == 20:
+        return "Parking lot road"
+    else:
+        return "Unknown"
 
-async def send_webhook(event, notify_type, message = ""):
+async def send_webhook(event, notify_type, title=""):
     async with aiohttp.ClientSession() as session:
-        embed = discord.Embed(title=message)
+        data = {
+            "token": pushover_api_token,
+            "user": pushover_user_key,
+            "title": f"{title}",
+            "message": f"WME: https://www.waze.com/en-US/editor?env=usa&lat={event['location']['y']}&lon={event['location']['x']}&zoomLevel=16, Livemap: https://www.waze.com/live-map/directions?to=ll.{event['location']['y']}%2C{event['location']['x']}",
+            "priority": 1 if notify_type == 1 else 0,
+            "sound": "climb" if notify_type == 1 else "cosmic",
+        }
+
+        await session.post("https://api.pushover.net/1/messages.json", data=data)
+
+        embed = discord.Embed(title=title)
         if notify_type == 0:
-            embed.add_field(name="Links", value=f"https://www.waze.com/en-US/editor?env=usa&lat={event['location']['y']}&lon={event['location']['x']}&zoomLevel=16")
+            embed.add_field(name="Links",
+                            value=f"https://www.waze.com/en-US/editor?env=usa&lat={event['location']['y']}&lon={event['location']['x']}&zoomLevel=16")
         webhook = discord.Webhook.from_url(discord_webhook_url, session=session)
         await webhook.send(embed=embed)
 
@@ -166,9 +164,9 @@ async def send_log(text):
         webhook = discord.Webhook.from_url(discord_webhook_log_url, session=session)
         await webhook.send(embed=embed)
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # asyncio.run(send_log("Script started"))
-    asyncio.run(start())
+    # asyncio.run(start())
 
     # while True:
     #     asyncio.run(start())
